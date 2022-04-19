dde961af28877fe32a1bfe6b2a13400b96d76162
salaphalghibrany@gmail.com <ahmad_salafudin_x@telkomsel.co.id>
2022-04-19 14:59:42 +0700

    Initial commit

---
+++ 1. Fundamental/59-Access Modifier dan Multi-File/src/alghibrany/util/Memasak.java
@@ -0,0 +1,20 @@
+package alghibrany.util;
+
+public class Memasak {
+
+    public static void dagingPublic(){
+        System.out.println("Alghibrany: Memasak: public");
+    }
+
+    private static void dagingPrivate(){
+        System.out.println("Alghibrany: Memasak: private");
+    }
+
+    static void dagingDefault(){
+        System.out.println("Alghibrany: Memasak: default");
+    }
+
+    protected static void dagingProtected(){
+        System.out.println("Alghibrany: Memasak: protected");
+    }
+}
